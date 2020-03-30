a7813901c8d94f504b58da646cbadad4a62563f3
Matt Canatella <matt.canatella@packet.com>
2020-03-30 10:42:15 -0500

    Minor alteration for naming clarity

--- bird/configure.py
+++ bird/configure.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import os
 import sys
 
 from packet_bird import Bird
 
-URL = os.getenv("URL", "https://metadata.packet.net/metadata")
+HELPER_URL = os.getenv("HELPER_URL", "https://metadata.packet.net/metadata")
 API_TOKEN = os.getenv("API_TOKEN", None)
 
 if __name__ == "__main__":
-    if URL != "https://metadata.packet.net/metadata" and not API_TOKEN:
+    if HELPER_URL != "https://metadata.packet.net/metadata" and not API_TOKEN:
         sys.exit("Packet API token missing from environment")
 
     headers = {}
     if API_TOKEN:
         headers["X-Auth-Token"] = API_TOKEN
 
-    r = Bird.http_fetch(URL, headers=headers)
+    r = Bird.http_fetch(HELPER_URL, headers=headers)
     print(r.config)
