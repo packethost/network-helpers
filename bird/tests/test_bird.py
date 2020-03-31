41183c8f00dbf161de8af66e460a2c9bb49f32a1
Matt Canatella <matt.canatella@packet.com>
2020-03-31 16:45:58 -0500

    Switched to pathlib and fixed some busted logic

--- bird/tests/test_bird.py
+++ bird/tests/test_bird.py
@@ -1,13 +1,10 @@
-import inspect
 import json
-import os
 import re
 import sys
-from os import listdir
-from os.path import isfile, join
-from typing import Dict, List
+from pathlib import Path
+from typing import Any, Dict, List, Tuple
 
-SESSIONS_PER_TYPE: Dict[int, int] = {
+SESSIONS_PER_TYPE: Dict[int, Dict[int, int]] = {
     1: {4: 1, 6: 0},
     2: {4: 0, 6: 1},
     3: {4: 1, 6: 1},
@@ -17,49 +14,57 @@ SESSIONS_PER_TYPE: Dict[int, int] = {
 }
 
 
-def initialize() -> List[str]:
-    currentdir = os.path.dirname(
-        os.path.abspath(inspect.getfile(inspect.currentframe()))
-    )
-    parentdir = os.path.dirname(currentdir)
-    sys.path.insert(0, parentdir)
-
-    data_path = join(currentdir, "data")
+def initialize(main: bool = False) -> Tuple[Dict[int, Any], List[Any]]:
+    current_dir = Path(__file__).parent.resolve()
+    data_dir = current_dir.joinpath("data").resolve()
+    if main:
+        parent_dir = current_dir.parent.resolve()
+        sys.path.insert(0, str(parent_dir))
 
     test_data = {}
     ip_addresses = []
-    for file in listdir(data_path):
-        absolute_filename = join(data_path, file)
-        if isfile(absolute_filename):
-            type_match = re.search(r"\/type([0-9]+)\.json$", absolute_filename)
-            if type_match:
-                with open(absolute_filename, "r") as fd:
-                    test_data[int(type_match.group(1))] = json.load(fd)
-            elif re.search(r"\/ip_addresses\.json$", absolute_filename):
-                with open(absolute_filename, "r") as fd:
-                    ip_addresses = json.load(fd)
+    data_files = [
+        str(item.resolve()) for item in Path(data_dir).glob("*.json") if item.is_file()
+    ]
+
+    for file in data_files:
+        type_match = re.search(r"\/type([0-9]+)\.json$", file)
+        if type_match:
+            with open(file, "r") as fd:
+                test_data[int(type_match.group(1))] = json.load(fd)
+        elif re.search(r"\/ip_addresses\.json$", file):
+            with open(file, "r") as fd:
+                ip_addresses = json.load(fd)
 
     return (test_data, ip_addresses)
 
 
-def test_Bird() -> None:
-    test_data, ip_addresses = initialize()
+def test_Bird(main: bool = False) -> None:
+    test_data, ip_addresses = initialize(main=main)
 
     from packet_bird import Bird
 
     for bgp_type, test in test_data.items():
         test["network"] = {"addresses": ip_addresses}
-        bird = Bird(**test)
 
         # Objects of type BirdNeighbor will self-validate when their
         # constructor is fired; hence we are only checking that each Bird
         # object has the correct number of bgp sessions
-        for neighbor in bird.bgp_neighbors:
-            assert (
-                len(neighbor.peer_ips)
-                == SESSIONS_PER_TYPE[bgp_type][neighbor.address_family]
-            )
+        bird = Bird(**test)
+        if len(bird.bgp_neighbors) == 1:
+            neighbor = bird.bgp_neighbors.pop()
+            if neighbor.address_family == 4:
+                assert SESSIONS_PER_TYPE[bgp_type][4] == len(neighbor.peer_ips)
+                assert SESSIONS_PER_TYPE[bgp_type][6] == 0
+            if neighbor.address_family == 6:
+                assert SESSIONS_PER_TYPE[bgp_type][4] == 0
+                assert SESSIONS_PER_TYPE[bgp_type][6] == len(neighbor.peer_ips)
+        else:
+            for i in range(len(bird.bgp_neighbors)):
+                assert SESSIONS_PER_TYPE[bgp_type][
+                    bird.bgp_neighbors[i].address_family
+                ] == len(bird.bgp_neighbors[i].peer_ips)
 
 
 if __name__ == "__main__":
-    test_Bird()
+    test_Bird(main=True)
