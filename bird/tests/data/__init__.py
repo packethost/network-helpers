7f10b91909e6281a464136d43d8d0bbb4fafdd70
Matt Canatella <matt.canatella@packet.com>
2020-04-03 08:30:33 -0500

    Got rid of json test data and implemented parametrize

---
+++ bird/tests/data/__init__.py
@@ -0,0 +1,220 @@
+from typing import Any, Dict, List
+
+IP_ADDRESSES: List[Dict[str, Any]] = [
+    {
+        "address": "147.75.65.31",
+        "address_family": 4,
+        "cidr": 31,
+        "customdata": {},
+        "enabled": True,
+        "gateway": "147.75.65.30",
+        "global_ip": None,
+        "manageable": True,
+        "management": True,
+        "netmask": "255.255.255.254",
+        "network": "147.75.65.30",
+        "public": True,
+    },
+    {
+        "address": "2604:1380:1:5f00::1",
+        "address_family": 6,
+        "cidr": 127,
+        "customdata": {},
+        "enabled": True,
+        "gateway": "2604:1380:1:5f00::",
+        "global_ip": None,
+        "manageable": True,
+        "management": True,
+        "netmask": "ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffe",
+        "network": "2604:1380:1:5f00::",
+        "public": True,
+    },
+    {
+        "address": "10.99.182.129",
+        "address_family": 4,
+        "cidr": 31,
+        "customdata": {},
+        "enabled": True,
+        "gateway": "10.99.182.128",
+        "global_ip": None,
+        "manageable": True,
+        "management": True,
+        "netmask": "255.255.255.254",
+        "network": "10.99.182.128",
+        "public": False,
+    },
+    {
+        "address": "10.99.182.254",
+        "address_family": 4,
+        "cidr": 32,
+        "customdata": {},
+        "enabled": True,
+        "gateway": "10.99.182.254",
+        "global_ip": None,
+        "manageable": True,
+        "management": False,
+        "netmask": "255.255.255.255",
+        "network": "10.99.182.254",
+        "public": False,
+    },
+]
+
+BGP_TYPES: List[Dict[str, Any]] = [
+    {
+        "bgp_neighbors": [
+            {
+                "address_family": 4,
+                "customer_as": 65000,
+                "customer_ip": "10.99.182.129",
+                "md5_enabled": False,
+                "md5_password": None,
+                "multihop": False,
+                "peer_as": 65530,
+                "peer_ips": ["169.254.255.1"],
+                "routes_in": [
+                    {"exact": False, "route": "10.1.0.0/31"},
+                    {"exact": False, "route": "10.2.0.0/29"},
+                ],
+                "routes_out": [],
+            }
+        ]
+    },
+    {
+        "bgp_neighbors": [
+            {
+                "address_family": 6,
+                "customer_as": 65000,
+                "customer_ip": "2604:1380:4111:2300::1",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": False,
+                "peer_as": 65530,
+                "peer_ips": ["fc00:0000:0000:0000:0000:0000:0000:000e"],
+                "routes_in": [
+                    {"exact": False, "route": "2604:1380:1:7400::/56"},
+                    {"exact": False, "route": "2604:1380:4111:2300::/56"},
+                ],
+                "routes_out": [],
+            }
+        ]
+    },
+    {
+        "bgp_neighbors": [
+            {
+                "address_family": 4,
+                "customer_as": 65000,
+                "customer_ip": "10.99.182.129",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": False,
+                "peer_as": 65530,
+                "peer_ips": ["169.254.255.1"],
+                "routes_in": [
+                    {"exact": False, "route": "10.1.0.0/31"},
+                    {"exact": False, "route": "10.2.0.0/29"},
+                ],
+                "routes_out": [],
+            },
+            {
+                "address_family": 6,
+                "customer_as": 65000,
+                "customer_ip": "2604:1380:4111:2300::1",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": False,
+                "peer_as": 65530,
+                "peer_ips": ["fc00:0000:0000:0000:0000:0000:0000:000e"],
+                "routes_in": [
+                    {"exact": False, "route": "2604:1380:1:7400::/56"},
+                    {"exact": False, "route": "2604:1380:4111:2300::/56"},
+                ],
+                "routes_out": [],
+            },
+        ]
+    },
+    {
+        "bgp_neighbors": [
+            {
+                "address_family": 4,
+                "customer_as": 65000,
+                "customer_ip": "10.99.182.129",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": True,
+                "peer_as": 65530,
+                "peer_ips": ["169.254.255.1", "169.254.255.2"],
+                "routes_in": [
+                    {"exact": False, "route": "10.1.0.0/31"},
+                    {"exact": False, "route": "10.2.0.0/29"},
+                ],
+                "routes_out": [],
+            }
+        ]
+    },
+    {
+        "bgp_neighbors": [
+            {
+                "address_family": 6,
+                "customer_as": 65000,
+                "customer_ip": "2604:1380:4111:2300::1",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": True,
+                "peer_as": 65530,
+                "peer_ips": [
+                    "fc00:0000:0000:0000:0000:0000:0000:000e",
+                    "fc00:0000:0000:0000:0000:0000:0000:000f",
+                ],
+                "routes_in": [
+                    {"exact": False, "route": "2604:1380:1:7400::/56"},
+                    {"exact": False, "route": "2604:1380:4111:2300::/56"},
+                ],
+                "routes_out": [],
+            }
+        ]
+    },
+    {
+        "bgp_neighbors": [
+            {
+                "address_family": 4,
+                "customer_as": 65000,
+                "customer_ip": "10.99.182.129",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": True,
+                "peer_as": 65530,
+                "peer_ips": ["169.254.255.1", "169.254.255.2"],
+                "routes_in": [
+                    {"exact": False, "route": "10.1.0.0/31"},
+                    {"exact": False, "route": "10.2.0.0/29"},
+                ],
+                "routes_out": [],
+            },
+            {
+                "address_family": 6,
+                "customer_as": 65000,
+                "customer_ip": "2604:1380:4111:2300::1",
+                "md5_enabled": True,
+                "md5_password": "ValidPassword123",
+                "multihop": True,
+                "peer_as": 65530,
+                "peer_ips": [
+                    "fc00:0000:0000:0000:0000:0000:0000:000e",
+                    "fc00:0000:0000:0000:0000:0000:0000:000f",
+                ],
+                "routes_in": [
+                    {"exact": False, "route": "2604:1380:1:7400::/56"},
+                    {"exact": False, "route": "2604:1380:4111:2300::/56"},
+                ],
+                "routes_out": [],
+            },
+        ]
+    },
+]
+
+
+def initialize() -> List[Dict[str, str]]:
+    for i in range(len(BGP_TYPES)):
+        BGP_TYPES[i]["network"] = {"addresses": IP_ADDRESSES}
+
+    return BGP_TYPES
--- bird/tests/test_bird.py
+++ bird/tests/test_bird.py
@@ -1,62 +1,35 @@
-import json
-import re
-from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict
 
-SESSIONS_PER_TYPE: Dict[int, Dict[int, int]] = {
-    1: {4: 1, 6: 0},
-    2: {4: 0, 6: 1},
-    3: {4: 1, 6: 1},
-    4: {4: 2, 6: 0},
-    5: {4: 0, 6: 2},
-    6: {4: 2, 6: 2},
-}
+import pytest
+from packet_bird import Bird
+from tests.data import initialize
+
+BGP_TYPES = initialize()
 
 
-def initialize() -> Tuple[Dict[int, Any], List[Any]]:
-    current_dir = Path(__file__).parent.resolve()
-    data_dir = current_dir.joinpath("data").resolve()
-
-    test_data = {}
-    ip_addresses = []
-    data_files = [
-        str(item.resolve()) for item in Path(data_dir).glob("*.json") if item.is_file()
-    ]
-
-    for file in data_files:
-        type_match = re.search(r"\/type([0-9]+)\.json$", file)
-        if type_match:
-            with open(file, "r") as fd:
-                test_data[int(type_match.group(1))] = json.load(fd)
-        elif re.search(r"\/ip_addresses\.json$", file):
-            with open(file, "r") as fd:
-                ip_addresses = json.load(fd)
-
-    return (test_data, ip_addresses)
-
-
-def test_Bird() -> None:
-    test_data, ip_addresses = initialize()
-
-    from packet_bird import Bird
-
-    for bgp_type, test in test_data.items():
-        test["network"] = {"addresses": ip_addresses}
-
-        # Objects of type BirdNeighbor will self-validate when their
-        # constructor is fired; hence we are only checking that each Bird
-        # object has the correct number of bgp sessions
-        bird = Bird(**test)
-        if len(bird.bgp_neighbors) == 1:
-            neighbor = bird.bgp_neighbors.pop()
-            if neighbor.address_family == 4:
-                assert SESSIONS_PER_TYPE[bgp_type][4] == len(neighbor.peer_ips)
-                assert SESSIONS_PER_TYPE[bgp_type][6] == 0
-            if neighbor.address_family == 6:
-                assert SESSIONS_PER_TYPE[bgp_type][4] == 0
-                assert SESSIONS_PER_TYPE[bgp_type][6] == len(neighbor.peer_ips)
-        else:
-            for i in range(len(bird.bgp_neighbors)):
-                assert SESSIONS_PER_TYPE[bgp_type][
-                    bird.bgp_neighbors[i].address_family
-                ] == len(bird.bgp_neighbors[i].peer_ips)
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (BGP_TYPES[0], {4: 1, 6: 0}),
+        (BGP_TYPES[1], {4: 0, 6: 1}),
+        (BGP_TYPES[2], {4: 1, 6: 1}),
+        (BGP_TYPES[3], {4: 2, 6: 0}),
+        (BGP_TYPES[4], {4: 0, 6: 2}),
+        (BGP_TYPES[5], {4: 2, 6: 2}),
+    ],
+)
+def test_Bird(test_input: Dict[str, Any], expected: Dict[int, int]) -> None:
+    bird = Bird(**test_input)
+    if len(bird.bgp_neighbors) == 1:
+        neighbor = bird.bgp_neighbors.pop()
+        if neighbor.address_family == 4:
+            assert expected[4] == len(neighbor.peer_ips)
+            assert expected[6] == 0
+        if neighbor.address_family == 6:
+            assert expected[4] == 0
+            assert expected[6] == len(neighbor.peer_ips)
+    else:
+        for j in range(len(bird.bgp_neighbors)):
+            assert expected[bird.bgp_neighbors[j].address_family] == len(
+                bird.bgp_neighbors[j].peer_ips
+            )
