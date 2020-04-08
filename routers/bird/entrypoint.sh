0700f2c28f8ce5799cc11ec06fb74b6c00126357
Matt Canatella <matt.canatella@packet.com>
2020-04-08 11:52:34 -0500

    Refactors continued

--- routers/bird/entrypoint.sh
+++ routers/bird/entrypoint.sh
@@ -5,10 +5,10 @@ set -eux
 # We should only start bird and bird6 if bgp over ipv4 and
 # ipv6 is respectively enabled
 
-/opt/bird/configure.py bird | tee /etc/bird/bird.conf
+/opt/routers/configure.py bird | tee /etc/bird/bird.conf
 if [ ${PIPESTATUS[0]} == 0 ]; then
-    echo >> /opt/bird/supervisord.conf
-    cat << EOF >> /opt/bird/supervisord.conf
+    echo >> /opt/routers/supervisord.conf
+    cat << EOF >> /opt/routers/supervisord.conf
 [program:bird]
 command = bird -c /etc/bird/bird.conf -d
 user = root
@@ -21,10 +21,10 @@ else
     rm -vf /etc/bird/bird.conf
 fi
 
-/opt/bird/configure.py bird6 | tee /etc/bird/bird6.conf
+/opt/routers/configure.py bird6 | tee /etc/bird/bird6.conf
 if [ ${PIPESTATUS[0]} == 0 ]; then
-    echo >> /opt/bird/supervisord.conf
-    cat << EOF >> /opt/bird/supervisord.conf
+    echo >> /opt/routers/supervisord.conf
+    cat << EOF >> /opt/routers/supervisord.conf
 [program:bird6]
 command = bird6 -c /etc/bird/bird6.conf -d
 user = root
@@ -33,9 +33,9 @@ sterr_logfile = /proc/1/fd/2
 stdout_logfile_maxbytes = 0
 stderr_logfile_maxbytes = 0
 EOF
-    echo >> /opt/bird/supervisord.conf
+    echo >> /opt/routers/supervisord.conf
 else
     rm -vf /etc/bird/bird6.conf
 fi
 
-supervisord -c /opt/bird/supervisord.conf
+supervisord -c /opt/routers/supervisord.conf
